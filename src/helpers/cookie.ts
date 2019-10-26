7ead2fdb192c894fe83800a4e4859be33076b1c5
gaoxuerong <gaoxuerong123@gmail.com>
2019-10-26 21:22:09 +0800

    xsrf

---
+++ src/helpers/cookie.ts
@@ -0,0 +1,7 @@
+const cookies = {
+  read(name: string): string | null {
+    const match = document.cookie.match(new RegExp('(^|;\\s*)(' + name + ')=([^;]*)'))
+    return match ? decodeURIComponent(match[3]) : null
+  }
+}
+export default cookies
