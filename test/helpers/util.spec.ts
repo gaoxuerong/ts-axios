4036c1d3c737664cc64578473f80c96ab3988b84
gaoxuerong <gaoxuerong123@gmail.com>
2019-11-02 22:23:21 +0800

    feat utils test init

--- test/helpers/util.spec.ts
+++ test/helpers/util.spec.ts
@@ -6,3 +6,11 @@ import {
   extend,
   deepMerge
 } from '../../src/helpers/utils'
+describe('helpers:utils', () => {
+  describe('isXX', () => {
+    // test('should validate Date, () => {
+    //   expect(isDate(new Date())).toBeTruthy()
+    //   expect(isDate(Date.now())).toBeFalsy()
+    // })
+  })
+})
