007d84dab2fb955680d7f7b38771cca63337abca
gaoxuerong <gaoxuerong123@gmail.com>
2019-11-07 21:26:14 +0800

    feat url test用例

--- test/helpers/url.spec.ts
+++ test/helpers/url.spec.ts
@@ -1,1 +1,11 @@
-import { buildURL, isAbsoluteURL, combineURL, isURLSameOrigin } from '../../src/helpers/url'
+import { isAbsoluteURL, combineURL, isURLSameOrigin } from '../../src/helpers/url'
+describe('helpers:url', () => {
+  describe('isURLSameOrigin', () => {
+    test('should detect same origin', () => {
+      expect(isURLSameOrigin(window.location.href)).toBeTruthy()
+    })
+    test('should detect different origin', () => {
+      expect(isURLSameOrigin('https://github.com/axios/axios')).toBeFalsy()
+    })
+  })
+})
