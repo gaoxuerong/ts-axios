7f5cb1884de463c5fd64b514e936f682f31a4101
gaoxuerong <gaoxuerong123@gmail.com>
2019-11-12 21:24:38 +0800

    feat add combineURL测试用例

--- test/helpers/url.spec.ts
+++ test/helpers/url.spec.ts
@@ -26,4 +26,25 @@ describe('helpers:url', () => {
       expect(isAbsoluteURL('foo')).toBeFalsy()
     })
   })
+  describe('combineURL', () => {
+    test('should combine URL', () => {
+      expect(combineURL('https://api.github.com', '/users')).toBe('https://api.github.com/users')
+    })
+
+    test('should remove duplicate slashes', () => {
+      expect(combineURL('https://api.github.com/', '/users')).toBe('https://api.github.com/users')
+    })
+
+    test('should insert missing slash', () => {
+      expect(combineURL('https://api.github.com', 'users')).toBe('https://api.github.com/users')
+    })
+
+    test('should not insert slash when relative url missing/empty', () => {
+      expect(combineURL('https://api.github.com/users', '')).toBe('https://api.github.com/users')
+    })
+
+    test('should allow a single slash for relative url', () => {
+      expect(combineURL('https://api.github.com/users', '/')).toBe('https://api.github.com/users/')
+    })
+  })
 })
