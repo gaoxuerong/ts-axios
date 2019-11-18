eaa9c1027ad9f4dd7def0dde4d9433e2d8880ea8
gaoxuerong <gaoxuerong123@gmail.com>
2019-11-19 07:42:14 +0800

    feat add test case helpers-headers

---
+++ test/helpers/headers.spec.ts
@@ -0,0 +1,23 @@
+import { parseHeaders } from '../../src/helpers/headers'
+describe('helper:headers', () => {
+  describe('parseHeaders', () => {
+    test('should parse headers', () => {
+      const parsed = parseHeaders(
+        'Content-Type: application/json\r\n' +
+          'Connection: keep-alive\r\n' +
+          'Transfer-Encoding: chunked\r\n' +
+          'Date: Tue, 21 May 2019 09:23:44 GMT\r\n' +
+          ':aa\r\n' +
+          'key:'
+      )
+      expect(parsed['content-type']).toBe('application/json')
+      expect(parsed['connection']).toBe('keep-alive')
+      expect(parsed['transfer-encoding']).toBe('chunked')
+      expect(parsed['date']).toBe('Tue, 21 May 2019 09:23:44 GMT')
+      expect(parsed['key']).toBe('')
+    })
+    test('should return empty object if headers is empty string', () => {
+      expect(parseHeaders('')).toEqual({})
+    })
+  })
+})
