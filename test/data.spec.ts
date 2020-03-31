861b30e4f501c3991bfbeada01f9c60781fa56a8
gaoxuerong <gaoxuerong123@gmail.com>
2020-03-31 21:45:59 +0800

    data test case frame

--- test/data.spec.ts
+++ test/data.spec.ts
@@ -1,1 +1,19 @@
 import { transformRequest, transformResponse } from '../src/helpers/data'
+describe('helpers:data', () => {
+  describe('transformRequest', () => {
+    test('', () => {
+      // todo
+    })
+    test('', () => {
+      // todo
+    })
+  })
+  describe('transformResponse', () => {
+    test('', () => {
+      // todo
+    })
+    test('', () => {
+      // todo
+    })
+  })
+})
